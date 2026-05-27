0175c0ce37155b1e1adcad8d62a6d12f1c7b0be6
Aditya Raj Singh <aditya@Adityas-MacBook-Air.local>
2026-05-27 23:31:54 +0530

    test

---
+++ proxy.ts
@@ -0,0 +1,25 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/login(.*)',
+  '/signup(.*)',
+  '/design-test',
+  '/robots.txt',
+  '/sitemap.xml',
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect()
+  }
+  return NextResponse.next()
+})
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
